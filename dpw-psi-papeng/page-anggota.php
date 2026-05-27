cae43564e7ec29f9cd87fb45bb32db61cc630433
IDejek <tombinawaiqbal@gmail.com>
2026-05-28 01:49:58 +0800

    Create page-anggota.php

---
+++ dpw-psi-papeng/page-anggota.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Template Name: Keanggotaan
+ *
+ * @package DPW_PSI_Papeng
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+// Redirect to PSI national membership page
+wp_safe_redirect( 'https://psi.id/menjadi-anggota', 301 );
+exit;
